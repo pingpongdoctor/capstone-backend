aa05e1d6664160fed480d50098d61731e15d678a
Simon Tran <thanhnhantran1501@gmail.com>
2022-12-02 11:51:17 -0800

    complete migration files

--- migrations/20221130190420_create_users_macros_recipes_tracking_tables.js
+++ migrations/20221130190420_create_users_macros_recipes_tracking_tables.js
@@ -10,7 +10,11 @@ exports.up = function (knex) {
         table.increments("id").primary();
         table.string("username").notNullable();
         table.string("email").notNullable();
-        table.string("password").notNullable;
+        table.string("password").notNullable();
+        table.string("gender").notNullable();
+        table.integer("age").notNullable();
+        table.decimal("weight").notNullable();
+        table.decimal("height").notNullable();
         table.timestamp("updated_at").defaultTo(knex.fn.now());
       })
       //MACROS TABLE
@@ -25,16 +29,13 @@ exports.up = function (knex) {
           .onUpdate("CASCADE")
           .onDelete("CASCADE");
         table.string("macro_name").notNullable();
-        table.decimal("weight").notNullable();
-        table.decimal("height").notNullable();
-        table.integer("age").notNullable();
-        table.integer("tdee").notNullable();
+        table.decimal("bmi").notNullable();
+        table.integer("bmr").notNullable();
+        table.decimal("targeted_weight").notNullable();
+        table.integer("calories_need").notNullable();
         table.decimal("pro").notNullable();
         table.decimal("carb").notNullable();
         table.decimal("fat").notNullable();
-        table.decimal("pro_percent");
-        table.decimal("carb_percent").notNullable();
-        table.decimal("fat_percent").notNullable();
         table.timestamp("updated_at").defaultTo(knex.fn.now());
       })
       //RECIPES TABLE
@@ -50,34 +51,17 @@ exports.up = function (knex) {
           .onDelete("CASCADE");
         table.string("recipe_name").notNullable();
         table.string("image").notNullable();
-        table.integer("likes").notNullable();
+        table.string("ingredients").notNullable();
+        table.string("description").notNullable();
+        table.string("step_1").notNullable();
+        table.string("step_2").notNullable();
+        table.string("step_3").notNullable();
+        table.string("step_4").notNullable().defaultTo("");
+        table.string("step_5").notNullable().defaultTo("");
+        table.string("step_6").notNullable().defaultTo("");
+        table.string("step_7").notNullable().defaultTo("");
+        table.integer("likes").notNullable().defaultTo("");
         table.table.timestamp("updated_at").defaultTo(knex.fn.now());
-      })
-      //INGREDIENTS TABLE
-      .createTable("ingredients", (table) => {
-        table.increments("id").primary();
-        table.string("ingredient_name").notNullable();
-        table.table.timestamp("updated_at").defaultTo(knex.fn.now());
-      })
-      //RECIPES_INGREDIENTS TABLE
-      .createTable("recipes_ingredients", (table) => {
-        table.increments("id").primary();
-        table
-          .integer("recipes_id")
-          .notNullable()
-          .unsigned()
-          .references("id")
-          .inTable("recipes")
-          .onUpdate("CASCADE")
-          .onDelete("CASCADE");
-        table
-          .integer("ingredients_id")
-          .notNullable()
-          .unsigned()
-          .references("id")
-          .inTable("ingredients")
-          .onUpdate("CASCADE")
-          .onDelete("CASCADE");
       })
       //RECIPE COMMENTS TABLE
       .createTable("recipes_comments", (table) => {
