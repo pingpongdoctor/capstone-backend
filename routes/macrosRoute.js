e324ba577d113a38aa36168d62a955132a5e7161
Simon Tran <thanhnhantran1501@gmail.com>
2022-12-09 15:16:50 -0800

    fisnish building routes to get all macros and to get a single macro

--- routes/macrosRoute.js
+++ routes/macrosRoute.js
@@ -2,5 +2,11 @@ const router = require("express").Router();
 const { checkToken } = require("../checkToken");
 const macrosController = require("../controllers/macrosController");
 
-router.route("/").post(checkToken, macrosController.postNewMacro);
+router
+  .route("/")
+  .post(checkToken, macrosController.postNewMacro)
+  .get(checkToken, macrosController.getAllMacros);
+
+router.route("/:id").get(checkToken, macrosController.getOneMacro);
+
 module.exports = router;
