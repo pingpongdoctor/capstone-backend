92561ae3493b65188f25a62c9ae14e48e1cc82f4
Simon Tran <thanhnhantran1501@gmail.com>
2022-12-27 13:59:47 -0800

    finish applying bcrypt to encrypt email and password

--- seeds_data/users.js
+++ seeds_data/users.js
@@ -2,8 +2,8 @@ module.exports = [
   {
     id: 1,
     username: "Simon",
-    email: "simon@gmail.com",
-    password: "12345",
+    email: "$2b$10$bqC3VplclkPNF8RZGoWiA.Zkt3poGI0ksuUwHVLqmqt560llzchXq",
+    password: "$2b$10$zxpy55zf/JRly4DSNUdhL.1sxMprqXCQRoI.jzcJuSk55FUN2WMuG",
     gender: "male",
     age: 29,
     weight: 80,
@@ -12,8 +12,8 @@ module.exports = [
   {
     id: 2,
     username: "Greg",
-    email: "greg@gmail.com",
-    password: "123456",
+    email: "$2b$10$CRURgcW/Ygv49Ao9Xa3Sz./W4REo3M74jNDci5ltTUf7edWc.rKK.",
+    password: "$2b$10$kcDjYUMzOI5yRVuLOk1lae8amGEVn1dMRIsx2q.tyJgemWJVXNsl.",
     gender: "male",
     age: 29,
     weight: 100,
