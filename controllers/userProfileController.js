79f3e33436c50fdea670e4da9d2d2d510ef5aa6a
Simon Tran <thanhnhantran1501@gmail.com>
2023-04-08 15:03:27 -0700

    update the userProfileController

--- controllers/userProfileController.js
+++ controllers/userProfileController.js
@@ -11,37 +11,36 @@ exports.getUserProfile = (req, res) => {
   }
 };
 
-exports.updateUserProfile = (req, res) => {
+exports.updateUserProfile = async (req, res) => {
   if (req.user) {
-    knex("users")
-      .where("id", req.user.id)
-      .update(req.body)
-      .then((data) => {
-        //CREATE A NEW JWT TOKEN BASED ON THE UPDATED USER PROFILE
-        knex("users").then((data) => {
-          const foundUser = data.find((user) => user.id === req.user.id);
-          if (foundUser) {
-            const { id, username, gender, age, weight, height, updated_at } =
-              foundUser;
-            const jwtToken = jwt.sign(
-              {
-                id: id,
-                username: username,
-                gender: gender,
-                age: age,
-                weight: weight,
-                height: height,
-                updated_at: updated_at,
-              },
-              JWT_SECRET
-            );
-            res.status(200).send(jwtToken);
-          }
-        });
-      })
-      .catch((error) => {
-        res.status(500).send("Can not update your profile");
-      });
+    try {
+      //UPDATE USER
+      await knex("users").where("id", req.user.id).update(req.body);
+      //GET THE UPDATED USER
+      const updatedUser = await knex("users").where("id", req.user.id);
+      console.log(updatedUser);
+      //CREATE A NEW JWT BASED ON NEW USER PROFILE
+      if (updatedUser[0]) {
+        const { id, username, gender, age, weight, height, updated_at } =
+          updatedUser[0];
+        const jwtToken = jwt.sign(
+          {
+            id,
+            username,
+            gender,
+            age,
+            weight,
+            height,
+            updated_at,
+          },
+          JWT_SECRET
+        );
+        console.log(jwtToken);
+        res.status(200).send(jwtToken);
+      }
+    } catch (error) {
+      res.status(500).send("Can not update your profile");
+    }
   } else {
     res.status(400).send("Make sure You have already log in!");
   }
